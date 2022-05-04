ade54bd67d4a26d5d1a79de6608ef7a66b195c69
RajrupDasid <rajrupdasid@outlook.com>
2022-05-04 16:54:50 +0530

    'main.py bug fixes and performance improvements'

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup #webscraping
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import datetime
-
+from decouple import config
+import smtplib
 now = datetime.datetime.now()
 #email content 
 content=''
@@ -16,7 +17,7 @@ def extract_news(url):
     cnt=''
     cnt+=('<b>HN Top Stories:</b>\n'+'<br>'+'-'*50+'<br>')
     response=requests.get(url)
-    content-response.content
+    content=response.content
     soup=BeautifulSoup(content,'html.parser')
     for i, tag in enumerate(soup.find_all('td',attrs={'class':'title','valign':''})):
         cnt+=((str(i+1)+' :: '+tag.text+"\n"+'<br>')if tag.text!='More' else '')
@@ -32,17 +33,17 @@ print('Processing for Composing an Email')
 #fill-out email details with required parameters
 SERVER='smtp.gmail.com'
 PORT=587
-FROM=''
-TO=''
-PASS='' #put password here
+FROM=config('FROM')
+TO=config('TO')
+PASS=config('PASS') #put password here
 #fp=open(file_name,'rb')
 #create a plain message
 #msg-MIMEText('')
 msg=MIMEMultipart()
 
 #msg.add_header('Content-Disposition','attachment','filename='empty.txt'')
-msg['Subject']='Hacker News Top stories'+'This is an automatic email-send using python'+''+str(now.d(now.year)+'-'+str(now.month)+'-'+str(now.day)
-msg['From']=FORM
+msg['Subject']='Hacker News Top stories'+'This is an auto-generated newsletter using python'+''+str(now.year)+'-'+str(now.month)+'-'+str(now.day)
+msg['From']=FROM
 msg['To']=TO
 
 msg.attach(MIMEText(content, 'html'))
@@ -51,3 +52,11 @@ msg.attach(MIMEText(content, 'html'))
 print('Initiating Server......')
 server=smtplib.SMTP(SERVER,PORT)
 #server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+#server.ehlo
+server.login(FROM,PASS)
+server.sendmail(FROM,TO,msg.as_string())
+print('Email sent....')
+server.quit()

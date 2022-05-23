c971309c79f201e38a5253cdd643324c53f2ffd8
RajrupDasid <rajrupdasid@outlook.com>
2022-05-23 10:27:47 +0530

    adding a new feature with new feature files | TED Talk video downloader

---
+++ master.py
@@ -0,0 +1,28 @@
+"""TED TALK DOWNLOADER WITH PYTHON"""
+import requests #getting content of the TED Talk page
+from bs4 import BeautifulSoup #web scraping
+import re #Regular pattern or expression matching
+#from urllib.request import urlretrieve 
+#for downloading mp4
+import sys #for argument parsing
+
+#Exception Handeling
+if len(sys.argv)>1:
+    url=sys.argv[1]
+else:
+    sys.exit("Error: While entering the URL | Please specify a correct one")
+#url=input("Enter your desired TED Talk url for apply download")
+
+r= requests.get(url)
+print("Processing Download")
+
+soup=BeautifulSoup(r.content,features="lxml")
+for val in soup.findAll("script"):
+    if (re.search("talkPage.init",str(val))) is not None:
+        result=str(val)
+result_mp4=re.search("(?P<url>https?://[\^s]+)(mp4)",result).group("url")
+mp4_url=result_mp4.split( '"' )[0]
+print("Downloading desire video from ....."+mp4_url)
+file_name=mp4_url.split("/")[len(mp4_url.split("/"))-1].split('?')[0]
+
+
